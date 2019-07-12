96226072da3144ff90392fa47b658a69b0b529ef
MajemiteJames <majemiteokoro@gmail.com>
2019-07-12 17:08:09 +0100

    resolving bug

--- server/index.js
+++ server/index.js
@@ -11,10 +11,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false,
 }));
-app.use('/api/v1', router);
 
 
-const PORT = process.env.PORT || 4040;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.status(200).json({
   status: 200,
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,18 +1,2 @@
-import express from 'express';
-import User from '../controllers/User';
-import signUpValidator from '../middleware/validators/signUpValidator';
-import signInValidator from '../middleware/validators/signInValidator';
-import Bus from '../controllers/bus';
-import TripController from '../controllers/TripController';
-import ValidationErrors from '../middleware/validators/errorMessages';
-import CreateTripMiddleware from '../middleware/CreateTripMiddleware';
-
-const router = express.Router();
 
 
-router.post('/signup', signUpValidator, User.create);
-router.post('/signin', signInValidator, User.login);
-router.post('/bus', Bus.create);
-router.post('/trips', CreateTripMiddleware, ValidationErrors, TripController.create);
-
-export default router;
