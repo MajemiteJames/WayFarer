9a4c78e7492de7ce9318d851768817d0f880958c
MajemiteJames <majemiteokoro@gmail.com>
2019-07-16 19:11:11 +0100

    ch(refactor code for auto grader)

--- server/controllers/Booking.js
+++ server/controllers/Booking.js
@@ -28,7 +28,7 @@ class BookingsController {
         ],
       };
       const getTrip = {
-        text: 'select * from trips2, buses where trips1.id = $1 AND trips2.busId = buses.id',
+        text: 'select * from trips2, buses where trips2.id = $1 AND trips2.busId = buses.id',
         values: [req.body.trip_id],
       };
 
