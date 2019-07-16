ffaf9a4d7bb5ed434bc2bf9017bae27826bcd0e2
MajemiteJames <majemiteokoro@gmail.com>
2019-07-16 14:32:46 +0100

    ch(refactor code for auto grader)

--- server/middleware/validators/signUpValidator.js
+++ server/middleware/validators/signUpValidator.js
@@ -2,10 +2,10 @@ import { check } from 'express-validator/check';
 import displayErrors from './errorMessages';
 
 const userInput = [
-  check('firstName').not().isEmpty().withMessage('First name cannot be empty'),
-  check('firstName').isAlpha().trim().withMessage('First name can only contain letters'),
-  check('lastName').not().isEmpty().withMessage('Last name cannot be empty'),
-  check('lastName').isAlpha().trim().withMessage('Last name can only contain letters'),
+  check('first_name').not().isEmpty().withMessage('First name cannot be empty'),
+  check('first_name').isAlpha().trim().withMessage('First name can only contain letters'),
+  check('last_name').not().isEmpty().withMessage('Last name cannot be empty'),
+  check('last_name').isAlpha().trim().withMessage('Last name can only contain letters'),
   check('email').not().isEmpty().withMessage('Email cannot be empty'),
   check('email').isEmail().trim().withMessage('Input a valid email address'),
   check('password').not().isEmpty().withMessage('Password cannot be empty'),
