0ac1b98b62b4bd94665e67c2743ade26d7797a1a
MajemiteJames <majemiteokoro@gmail.com>
2019-07-16 15:58:18 +0100

    ch(refactor code for auto grader)

--- server/controllers/Trips.js
+++ server/controllers/Trips.js
@@ -76,7 +76,7 @@ class Trips {
     }
 
     status = isEmpty(status) ? 'active' : status;
-    const text = `INSERT INTO trips(busId, origin, destination, tripDate, fare, status) 
+    const text = `INSERT INTO trips(bus_id, origin, destination, tripDate, fare, status) 
     VALUES($1, $2, $3, $4, $5, $6) returning * ;
     `;
     const values = [bus_id, origin, destination, trip_date, fare, status];
